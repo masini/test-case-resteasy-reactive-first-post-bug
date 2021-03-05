5bdf84883f59775947fab331699f7ed8799810c0
Luca Masini <luca.masini@gmail.com>
2021-03-05 09:06:41 +0100

    Added an end-point to simulate the bug

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,10 +1,10 @@
 package org.acme;
 
 import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 
 @Path("/test")
@@ -15,6 +15,16 @@ public class GreetingResource {
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed("TERMINAL_ROLE")
     public TestDTO save(TestDTO n) {
+
         return n;
+    }
+
+    @GET
+    public void call() {
+        Client client = ClientBuilder.newClient();
+
+        client.target("http://localhost:8082/resources/test")
+                .request().accept(MediaType.APPLICATION_JSON_TYPE).header("Authorization", "Basic YWRtaW46YWRtaW4=")
+                .post(Entity.entity("{}", MediaType.APPLICATION_JSON_TYPE));
     }
 }
